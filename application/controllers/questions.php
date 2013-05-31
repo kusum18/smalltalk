1d7f394fa70bf3c3601bcd6e73eac344735e5975
Nirav <niravv@uci.edu>
2013-05-31 05:45:04 -0700

    self

--- application/controllers/questions.php
+++ application/controllers/questions.php
@@ -28,53 +28,88 @@ class Questions extends REST_Controller {
 		
 		//temp storage array
 		$friend = array();
-
+		$flag =0;
 		foreach($friedndObj->all as $o)
 		{
+			$flag = 1;
 			$friend['friend_id'][]=$o->friend_id;	
 		}
 		
-		//fetch data from User table only the users which are registed in the poocho app
-		$userObj->select('id');
-		$userObj->where('isRegistered',1);
-		$userObj->where_in('id',$friend['friend_id']);
-		$userObj->get(); 
-		
-		//temp storage array
-		$userReg =array();
-		
-		foreach($userObj->all as $o)
+		if ($flag ==0)
 		{
-			$userReg['users'][]=$o->id;
-			
-		}	
+			$friend['status_code'] = 400;
+			$this->response($friend);
 		
-		//fetching the questions whose user id is $userid
-		$postObj->where_in('user_id',$userReg['users']);
-		$postObj->where('post_type',1);
-		$postObj->order_by('id','desc');
-		$postObj->get($totalrecords, $start);
-		
-		//array to store the questions
-		$info = array();
-		$finalifno= array();
-		
-		//loop to store data into array
-		foreach ($postObj->all as $obj)
-		{
-			$info['post_id']=$obj->id;
-			$info['post_text']=$obj->post_text;
-			$info['post_title']=$obj->title;
-			$info['accepted_answer']=$obj->question_answer_id;
-			$tempuserObj->where('id',$obj->user_id)->get();
-			$info['owner_name']=$tempuserObj->username;
-			$info['owner_id']=$tempuserObj->id;
-			$finalifno['questions'][]=$info;
-			
 		}
 		
-		//printing the result
-		$this->response($finalifno); 
+		else
+		
+		{
+		
+			//fetch data from User table only the users which are registed in the poocho app
+			$userObj->select('id');
+			$userObj->where('isRegistered',1);
+			$userObj->where_in('id',$friend['friend_id']);
+			$userObj->get(); 
+			
+			//temp storage array
+			$userReg =array();
+			$flag  =0;
+			foreach($userObj->all as $o)
+			{
+				$flag =1;
+				$userReg['users'][]=$o->id;
+				
+			}	
+			if($flag==0)
+			{
+				$friend['status_code'] = 400;
+				$this->response($friend);
+			
+			}
+			else
+			{
+			
+				//fetching the questions whose user id is $userid
+				$postObj->where_in('user_id',$userReg['users']);
+				$postObj->where('post_type',1);
+				$postObj->order_by('id','desc');
+				$postObj->get($totalrecords, $start);
+				
+				//array to store the questions
+				$info = array();
+				$finalifno= array();
+				
+				$flag = 0;
+				//loop to store data into array
+				foreach ($postObj->all as $obj)
+				{
+					$flag=1;
+					$info['post_id']=$obj->id;
+					$info['post_text']=$obj->post_text;
+					$info['post_title']=$obj->title;
+					$info['accepted_answer']=$obj->question_answer_id;
+					$tempuserObj->where('id',$obj->user_id)->get();
+					$info['owner_name']=$tempuserObj->username;
+					$info['owner_id']=$tempuserObj->id;
+					$finalifno['questions'][]=$info;
+					
+				}
+				if ($flag==1)
+				{
+					$finalifno['status_code']=200;
+					//printing the result
+					$this->response($finalifno); 
+				}
+				
+				else
+				{
+					$finalifno['status_code']=400;
+					//printing the result
+					$this->response($finalifno);
+				}
+			}
+		}
 
 	
 	
