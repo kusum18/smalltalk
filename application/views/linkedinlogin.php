2a763026546673e8840638aa83c5bdce9dff4da9
Nirav <niravv@uci.edu>
2013-06-14 03:41:29 -0700

    link

--- application/views/linkedinlogin.php
+++ application/views/linkedinlogin.php
@@ -5,13 +5,10 @@
 <h1></h1>
 
 <p></p>
-<<<<<<< HEAD
+
 <form action="http://54.214.205.250/smalltalk/index.php/linktest/authcode">
-    <input type="submit" value="Cennect Linkedin">
-=======
-<form action="http://localhost/smalltalk/index.php/linktest/authcode">
     <input type="submit" value="Connect Linkedin">
->>>>>>> f3d4b723a1c7a709018d10e04ecd5cc9a9216dae
+
 </form>
 
 </body>
